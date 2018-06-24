611888d69ac980ce99d8bed2e5f10f1443697d6e
Ruaridh Watt <ruaridh.watt@omegapoint.se>
2018-06-24 14:23:09 +0200

    v-if

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,19 @@
 <template>
   <v-flex xs12 sm6 offset-sm3>
     <v-card>
-      <v-card-title>
-        <span class="headline">HellowWorld</span>
-      </v-card-title>
       <v-card-text>
         <p>{{ msg }}</p>
+        <VIf></VIf>
       </v-card-text>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+import VIf from './VIf'
 export default {
   name: 'HelloWorld',
+  components: {VIf},
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
---
+++ src/components/VIf.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="headline">v-conditional</span>
+    </v-card-title>
+    <v-card-text>
+      <p v-if="showPlums && showPeaches">Plums and peaches</p>
+      <p v-else-if="showPlums">Plums no peaches</p>
+      <p v-else-if="showPeaches">Peaches no plums</p>
+      <p v-else>No plums or peaches</p>
+      <p v-show="showPlums && showPeaches">Html display:none if no plums and peaches</p>
+      <v-switch
+        label="Plums"
+        v-model="showPlums"
+      ></v-switch>
+      <v-switch
+        label="Peaches"
+        v-model="showPeaches"
+      ></v-switch>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'VIf',
+  data () {
+    return {
+      showPlums: true,
+      showPeaches: true
+    }
+  }
+}
+
+</script>
