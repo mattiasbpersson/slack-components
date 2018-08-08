1daf8ec6a5704e1cc7a38f6346de95451b9b98f1
Ruaridh Watt <ruaridh.watt@omegapoint.se>
2018-08-08 15:59:11 +0200

    Put in some more info in the already done examples.

--- src/components/VFor.vue
+++ src/components/VFor.vue
@@ -1,16 +1,26 @@
 <template>
   <v-card>
+
     <v-card-title>
-      <span class="headline">VFor</span>
+      <span class="headline">
+        <code>v-for</code>
+      </span>
     </v-card-title>
+
     <v-card-text>
+      <p>
+        Repeating markup for each item in an array.
+      </p>
+
       <template v-for="fruit in fruits">
         <p v-bind:key="fruit.name"
            v-bind:class="{ripeFruit: fruit.isRipe}">
           {{fruit.name}}
         </p>
       </template>
+
     </v-card-text>
+
   </v-card>
 </template>
 
