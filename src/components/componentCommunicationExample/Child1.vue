6bb9fe9dcbd3a695cda57a03b4847b3287ecea5c
Ruaridh Watt <ruaridh.watt@omegapoint.se>
2018-08-08 16:00:01 +0200

    Added a component communication example and updated todo list

---
+++ src/components/componentCommunicationExample/Child1.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="headline">
+        Child 1
+      </span>
+    </v-card-title>
+
+    <v-card-actions>
+
+      <v-text-field
+        label="Write something..."
+        :value="text"
+        @input="$emit('textChanged', $event)"
+      ></v-text-field>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Child1',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+  v-card {
+    background-color: lightblue;
+  }
+</style>
---
+++ src/components/componentCommunicationExample/Child2.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <span class="headline">
+        Child 2
+      </span>
+    </v-card-title>
+
+    <v-card-text>
+      {{ text }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Child2',
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
