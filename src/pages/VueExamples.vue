cfc547d2a533e08c08eb9c55d05784cb1dc4d41c
Ruaridh Watt <ruaridh.watt@omegapoint.se>
2018-08-15 19:43:03 +0200

    v-model with custom components

--- src/pages/VueExamples.vue
+++ src/pages/VueExamples.vue
@@ -5,7 +5,7 @@
       <v-card-title class="headline">{{ headline }}</v-card-title>
 
       <v-card-text>
-
+        Check out this cool <a href="https://vuejs-tips.github.io/cheatsheet/">cheat sheet</a> <br>
         <VIf></VIf>
         <br/>
 
@@ -19,6 +19,9 @@
         <br/>
 
         <EventBusBasedCommunication/>
+        <br/>
+
+        <VModel/>
 
       </v-card-text>
     </v-card>
@@ -31,10 +34,11 @@ import VFor from '../components/examples/VFor'
 import VBind from '../components/examples/v-bindExample/VBind'
 import PropAndEventBasedCommunication from '../components/examples/communicationExample/PropAndEventBasedCommunication'
 import EventBusBasedCommunication from '../components/examples/eventBusExample/EventBusBasedCommunication'
+import VModel from '../components/examples/v-modelExample/VModel'
 
 export default {
   name: 'VueExamples',
-  components: {EventBusBasedCommunication, PropAndEventBasedCommunication, VBind, VFor, VIf},
+  components: {EventBusBasedCommunication, PropAndEventBasedCommunication, VBind, VFor, VIf, VModel},
   data () {
     return {
       headline: 'Some Vue.js examples'
