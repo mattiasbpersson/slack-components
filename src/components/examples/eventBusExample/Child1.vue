c4f6d05cc6721703c94e1fa1e037d925b07216be
Ruaridh Watt <ruaridh.watt@omegapoint.se>
2018-08-21 10:26:13 +0200

    Removed unnecessary specification of $event argument.

--- src/components/examples/eventBusExample/Child1.vue
+++ src/components/examples/eventBusExample/Child1.vue
@@ -11,7 +11,7 @@
       <v-text-field
         label="Write something..."
         :value="text"
-        @input="textChanged($event)"
+        @input="textChanged"
       ></v-text-field>
     </v-card-actions>
   </v-card>
